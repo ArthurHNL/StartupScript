a036f012a88f6d4e20e0d1580f8f1c44b6dc870e
Arthur Heidt <arthur@arthurheidt.nl>
2019-03-03 15:18:46 +0100

    add pageant launch

--- ArthurHNL.StartupScript.App/Program.fs
+++ ArthurHNL.StartupScript.App/Program.fs
@@ -1,6 +1,13 @@
 ﻿open ArthurHNL.StartupScript.Lib.ClearFolders
+open ArthurHNL.StartupScript.Lib.LaunchPrograms
 
 [<EntryPoint>]
 let main argv =
     clearFolder "C:\\Copyfolder" true
+    let pageant : LaunchInfo = { 
+        executablePath = "C:\\Program Files\\PuTTY\\pageant.exe";
+        arguments = "C:\\Users\\arthu\.ssh\id_rsa.ppk";
+        workingDirectory = "C:\\"; }
+    let executables = [pageant]
+    launchPrograms executables
     0
---
+++ ArthurHNL.StartupScript.Lib/LaunchPrograms.fs
@@ -0,0 +1,17 @@
+﻿namespace ArthurHNL.StartupScript.Lib
+
+open System.Diagnostics
+
+module LaunchPrograms =
+    type LaunchInfo = { executablePath : string; arguments : string; workingDirectory : string }
+
+    let private launchProgram programInfo =
+        let launchInfo = ProcessStartInfo(FileName = programInfo.executablePath, Arguments = programInfo.arguments, WorkingDirectory = programInfo.workingDirectory)
+        Process.Start(launchInfo) |> ignore
+
+    let rec launchPrograms programsToLaunch =
+        match programsToLaunch with
+        | head :: tail ->
+            launchProgram head
+            launchPrograms tail
+        | [] -> ignore
