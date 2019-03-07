93b67aacec5fc081136836d8287a00859b62f150
Arthur Heidt <arthur@arthurheidt.nl>
2019-03-07 17:59:50 +0100

    clearfolders now uses seq.iter

--- ArthurHNL.StartupScript.Lib/ClearFolders.fs
+++ ArthurHNL.StartupScript.Lib/ClearFolders.fs
@@ -7,11 +7,19 @@ module ClearFolders =
     let rec clearFolder path clearSubdirs =
         if not <| Directory.Exists(path) then raise (DirectoryNotFoundException(path))
 
-        for file in Directory.EnumerateFiles(path) do
-            File.Delete(Path.Combine(path, file))
+        Seq.iter 
+            (fun filePath -> File.Delete(filePath))
+            (Seq.map
+                (fun file -> Path.Combine(path, file))
+                (Directory.EnumerateFiles(path)))
 
-        if clearSubdirs then
-            for subdir in Directory.EnumerateDirectories(path) do
-                let fullPath = Path.Combine(path, subdir)
-                clearFolder fullPath clearSubdirs
-                Directory.Delete(fullPath)
+        match clearSubdirs with 
+        | true ->
+            Seq.iter
+                (fun subdir -> 
+                    clearFolder subdir clearSubdirs
+                    Directory.Delete(subdir))
+                (Seq.map
+                    (fun subdir -> Path.Combine(path, subdir))
+                    (Directory.EnumerateDirectories(path)))
+        | _ -> ()
